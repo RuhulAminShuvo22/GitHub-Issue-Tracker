1db87fa1d9e0adf7ecd16f9c6d4e4b7b30875dad
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-09 00:18:48 +0600

    load section done

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function login() {
     alert("Invalid Credentials");
   }
 }
+
+
+/* ================= LOAD ISSUES ================= */
+
+async function loadIssues() {
+  setActive("allTab");
+
+  document.getElementById("loader").classList.remove("hidden");
+
+  const res = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+
+  const data = await res.json();
+
+  allIssues = data.data;
+
+  displayIssues(allIssues);
+
+  document.getElementById("loader").classList.add("hidden");
+}
+
+/* Auto load */
+
+if (document.getElementById("issuesContainer")) {
+  loadIssues();
+}
